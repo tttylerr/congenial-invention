ddd3ebdb3482c1227d75f5108edb1966ed5f5e2a
GamingLizard9 <dawsoncall888@gmail.com>
2022-06-28 11:08:04 -0400

    links (maybe?)

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
 
+app.get('/docs', (req, res) => {
+    res.sendFile(path.resolve('pages/docs.html'));
+});
+  
+app.get('/projects', (req, res) => {
+    res.sendFile(path.resolve('pages/projects.html'));
+});
+  
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve('pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve('pages/contact.html'));
+});
+
+app.get('/issue', (req, res) => {
+    res.sendFile(path.resolve('pages/report.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at port ${port}`);
 });
